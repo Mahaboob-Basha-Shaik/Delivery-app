a0d807cfd29b884bd6f1412a0ed7cd9950843eab
Mahaboob Shaik <149460386+Mahaboob-Basha-Shaik@users.noreply.github.com>
2025-09-18 13:52:39 +0530

    Delete AmazonClone directory

--- AmazonClone/src/main/java/com/amazonclone/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.amazonclone.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.amazonclone.model.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
--- AmazonClone/src/main/java/com/amazonclone/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.amazonclone.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.amazonclone.model.User;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	Optional<User> findByEmail(String email); // For login functionality
-}
